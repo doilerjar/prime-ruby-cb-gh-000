0333733142e48fca01aa9353a3c0a63ba0fce2ae
Kevin K <doilerjar@outlook.com>
2018-08-08 00:59:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(n)
+  if n < 2 
+    return false 
+  end 
+  
+  if n == 2 || n == 3
+    return true 
+  else 
+    a = (2..n-1).to_a
+    # binding.pry 
+    a.each do |num|
+      if n % num == 0 
+        return false
+      end 
+    end 
+  end 
+  return true 
+end 
