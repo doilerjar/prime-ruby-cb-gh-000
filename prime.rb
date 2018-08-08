492bdd997688af09fe45cbc67835c61760c441f0
Kevin K <doilerjar@outlook.com>
2018-08-08 00:49:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,12 @@
+require 'pry'
 # Add  code here!
 def prime?(n)
   if n == 1 || n == 2 | n == 3
     return true 
   else 
-    a = 
+    a = (2..n-1).to_a
+    binding.pry 
+  end 
 end 
+
+prime(5)
